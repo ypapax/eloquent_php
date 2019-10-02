f6a727da81466049a4639008ffd82b3170b0b328
Maxim Yefremov <maxYefr@gmail.com>
2019-10-02 14:08:13 +0300

    migrate to postgres

--- commands.sh
+++ commands.sh
@@ -2,20 +2,42 @@
 set -ex
 set -o pipefail
 
-generate(){
+generate() {
   laravel new first_laravel_site
 }
 
-dev(){
+mig() {
+  php artisan migrate
+}
+
+dev() {
+  mig
   php artisan serve
 }
 
-queryStr(){
+queryStr() {
   curl http://localhost:8000/?name=Goodwin
 }
 
-controller(){
+controller() {
   php artisan make:controller PagesController
+}
+
+compFile=docker-compose.yml
+
+post() {
+  docker-compose -f $compFile build
+  docker-compose -f $compFile up
+}
+
+repost() {
+  set +e
+  for c in eloquent_php_postgres; do
+    docker kill $c
+    docker rm $c
+  done
+  set -e
+  post
 }
 
 "$@"
